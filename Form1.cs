737e5c23006110106cacf85e0f607afac7b3906e
DenArs2719 <yevseyenkod@mail.ru>
2020-04-21 10:21:43 +0200

    Stworzenie projektu, użycie komponentu do pomiaru czasu (Timer), zdefiniowanie i obsługa własnego zdarzenia (delegate, event)

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        SnakeLogic mySnakeLogic;
+        public Form1()
+        {
+            InitializeComponent();
+            mySnakeLogic = new SnakeLogic();
+            mySnakeLogic.SnakeMoved += MySnakeLogic_SnakeMoved;
+        }
+
+        private void MySnakeLogic_SnakeMoved()
+        {
+            this.Text += "#";
+        }
+    }
+}
---
+++ SnakeLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class SnakeLogic
+    {
+        public delegate void SnakeEvent(); ///pokazanie pewnego typu metody
+        public event SnakeEvent SnakeMoved; /// dekloracja eventa typu SnakeEvent()
+
+        Timer timer = new Timer(); ///deklaracja timera
+
+        public SnakeLogic()
+        {
+            ///konfiguracja taimera
+            timer.Enabled = true;
+            timer.Interval = 200;
+
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(SnakeMoved != null) ///zeby nie było błąda na wartość null
+            {
+                SnakeMoved(); ///wywolujemy zdarzenie
+            }
+
+        }
+    }
+}
